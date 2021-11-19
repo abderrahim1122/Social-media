ca84fffe55aa2e46548c0ec3664ef59cc86a12ee
Abderrahim Chakhmoun <a.chakhmoun98@gmail.com>
2021-11-19 23:36:45 +0100

    Init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <link href="style.css" rel="stylesheet" media="all">
+    
+    <div class="container">
+        <div class="jumbotron">
+            <h1>Bienvenue sur Voyager+</h1>
+            <p>un réseau social de partage d'expériences de voyage </p>
+        </div>
+    </div>
+
+@endsection
